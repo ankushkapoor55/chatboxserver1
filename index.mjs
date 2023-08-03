0d04e0785b462295d2380e8ca1d5e71fb77f4c5f
Ankush Kapoor <117109190+ankushkapoor55@users.noreply.github.com>
2023-08-03 18:30:34 +0530

    Add files via upload

---
+++ index.mjs
@@ -0,0 +1,44 @@
+import { createServer } from "http";
+import express from "express";
+import cors from "cors";
+import { Server } from "socket.io";
+
+const app=express();
+const port= 5000 || process.env.PORT ;
+
+
+const users=[{}];
+
+app.use(cors());
+app.get("/",(req,res)=>{
+    res.send("HELL ITS WORKING");
+})
+
+const server=createServer(app);
+
+const io = new Server(server);
+
+io.on("connection",(socket)=>{
+    console.log("New Connection");
+
+    socket.on('joined',({user})=>{
+          users[socket.id]=user;
+          console.log(`${user} has joined `);
+          socket.broadcast.emit('userJoined',{user:"Admin",message:` ${users[socket.id]} has joined`});
+          socket.emit('welcome',{user:"Admin",message:`Welcome to the chat,${users[socket.id]} `})
+    })
+
+    socket.on('message',({message,id})=>{
+        io.emit('sendMessage',{user:users[id],message,id});
+    })
+
+    socket.on('disconnect',()=>{
+          socket.broadcast.emit('leave',{user:"Admin",message:`${users[socket.id]}  has left`});
+        console.log(`user left`);
+    })
+});
+
+
+server.listen(port,()=>{
+    console.log(`Working`);
+})
